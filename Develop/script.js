36f89605c0ddc8e84c48d0080d22ceaa6d60c27b
Austin W <austinwoo123@gmail.com>
2021-01-05 17:45:53 -0800

    fix the link to jquery and moment. and edit the function saveDay/saveBtn

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ var time10 = $("#10");
 var time11 = $("#11");
 var time12 = $("12");
 var time1 = $("#13");
-var time2 = $("#14)";
+var time2 = $("#14");
 var time3 = $("#15");
 var time4 = $("#16");
 var time5 = $("#17");
@@ -13,6 +13,31 @@ var time5 = $("#17");
 var time = moment();
 
 function saveDay() {
-    $("#currentDay").text(moment().format("")
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+    $(".description").each(function () {
+        var id = $(this).parent().attr("id");
+        console.log(id)
+        var description = localStorage.getItem(id);
+        console.log(description)
+        if (description !== null) {
+            $(this).val(description);
+        }
+    });
 }
+
+saveDay();
+
+var saveBtn = $(".saveBtn");
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var description = $(this).siblings(".description").val()
+    console.log(time)
+    console.log(description)
+    localStorage.setItem(time, description);
+
+
+
+});
+
 
