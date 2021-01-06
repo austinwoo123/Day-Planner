9908d4cfdb854e6e1802af33310f5228ed8a0279
Austin W <austinwoo123@gmail.com>
2021-01-05 16:52:36 -0800

    begin script and define variables for 9am-5pm and create function to save what we did during that day

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,18 @@
+
+// Set variables for each time //
 var time9 = $("#9");
 var time10 = $("#10");
 var time11 = $("#11");
+var time12 = $("12");
+var time1 = $("#13");
+var time2 = $("#14)";
+var time3 = $("#15");
+var time4 = $("#16");
+var time5 = $("#17");
+
+var time = moment();
+
+function saveDay() {
+    $("#currentDay").text(moment().format("")
+}
+
